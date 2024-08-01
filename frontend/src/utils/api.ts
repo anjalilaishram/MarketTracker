9cdc792e5caf4657e87c10229f4c219ae0a82b1d
anjalilaishram <anjali.laishram.career@gmail.com>
2024-08-01 19:53:17 +0530

    added .env.local

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,6 @@
-
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:5000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const fetchHistoricalData = async (symbol: string, startTime: number, endTime: number) => {
   const response = await axios.get(`${API_URL}/data/historical/${symbol}`, {
